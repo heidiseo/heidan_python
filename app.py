9f565166b49a4f7d9a260243ef2a9a27fd8b7aeb
Heidi Seo <heidiseo1@gmail.com>
2020-11-10 21:28:19 +0000

    WIP - add USER db

--- app.py
+++ app.py
@@ -14,8 +14,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 # not to have the warning
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init db
-db = SQLAlchemy(app)
+# Init db for activity and user
+activity_db = SQLAlchemy(app)
+user_db = SQLAlchemy(app)
 # Init marshmallow
 ma = Marshmallow(app)
 
@@ -29,15 +30,15 @@ activity_schema = ActivitySchema()
 
 activities_schema = ActivitySchema(many=True)
 
-# Product Class/Model
-class Activity(db.Model):
-    __tablename__ = 'activity'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    location = db.Column(db.String(300))
-    description = db.Column(db.String(200))
-    cost = db.Column(db.Float)
-    complete = db.Column(db.Boolean)
+# Activity Class/Model
+class Activity(activity_db.Model):
+    __tablename__ = 'activities'
+    id = activity_db.Column(activity_db.Integer, primary_key=True)
+    name = activity_db.Column(activity_db.String(100), unique=True)
+    location = activity_db.Column(activity_db.String(300))
+    description = activity_db.Column(activity_db.String(200))
+    cost = activity_db.Column(activity_db.Float)
+    complete = activity_db.Column(activity_db.Boolean)
 
     def __init__(self, name, location, description, cost, complete):
         self.name = name
@@ -58,8 +59,8 @@ def add_activity():
 
     new_activity = Activity(name, location, description, cost, complete)
 
-    db.session.add(new_activity)
-    db.session.commit()
+    activity_db.session.add(new_activity)
+    activity_db.session.commit()
 
     return activity_schema.jsonify(new_activity)
 
@@ -97,7 +98,7 @@ def update_activity(id):
     activity.cost = cost
     activity.complete = complete
 
-    db.session.commit()
+    activity_db.session.commit()
 
     return activity_schema.jsonify(activity)
 
@@ -106,9 +107,99 @@ def update_activity(id):
 @cross_origin()
 def delete_activity(id):
     activity = Activity.query.get(id)
-    db.session.delete(activity)
-    db.session.commit()
+    activity_db.session.delete(activity)
+    activity_db.session.commit()
+
     return activity_schema.jsonify(activity)
+
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields=('id', 'first_name', 'last_name', 'email', 'photo_url')
+
+
+# Init schema
+user_schema = UserSchema()
+
+users_schema = UserSchema(many=True)
+
+# User Model
+class User(user_db.Model):
+    __tablename__ = 'users'
+    id = user_db.Column(user_db.Integer, primary_key=True)
+    first_name = user_db.Column(user_db.String(30))
+    last_name = user_db.Column(user_db.String(30))
+    email = user_db.Column(user_db.String(50))
+    photo_url = user_db.Column(user_db.String(200))
+
+    def __init__(self, first_name, last_name, email, photo_url):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self. photo_url = photo_url
+
+# Create a User
+@app.route('/user', methods=['POST'])
+@cross_origin()
+def add_user():
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    photo_url = request.json['photo_url']
+
+    new_user = User(first_name, last_name, email, photo_url)
+
+    user_db.session.add(new_user)
+    user_db.session.commit()
+
+    return user_schema.jsonify(new_user)
+
+
+# Get ALL users
+@app.route('/users', methods=['GET'])
+@cross_origin()
+def get_users():
+    all_users = User.query.all()
+    result = users_schema.dump(all_users)
+    return jsonify(result)
+
+# Get a single user
+@app.route('/user/<id>', methods=['GET'])
+@cross_origin()
+def get_user(id):
+    user = User.query.get(id)
+    return user_schema.jsonify(user)
+
+# Update a User
+@app.route('/user/<id>', methods=['PUT'])
+@cross_origin()
+def update_user(id):
+    user = User.query.get(id)
+
+    first_name = request.json['first_name']
+    last_name = request.json['last_name']
+    email = request.json['email']
+    photo_url = request.json['photo_url']
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.email = email
+    user.photo_url = photo_url
+
+    user_db.session.commit()
+
+    return user_schema.jsonify(user)
+
+# Delete a User
+@app.route('/user/<id>', methods=['DELETE'])
+@cross_origin()
+def delete_user(id):
+    user = User.query.get(id)
+    user_db.session.delete(user)
+    user_db.session.commit()
+
+    return user_schema.jsonify(user)
 
 # Run Server
 if __name__ == '__main__':
--- manage.py
+++ manage.py
@@ -3,9 +3,9 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
-from app import app, db
+from app import app, activity_db
 
-migrate = Migrate(app, db)
+migrate = Migrate(app, activity_db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
